2534b1ed8e95b54e9826f7144a0e3783ea237ad6
KS <dagda@hanafos.com>
2026-02-27 10:18:38 +0900

    modify Testmain.java

--- TestMain.java
+++ TestMain.java
@@ -1,6 +1,7 @@
 public class TestMain {
     public static void main(String[] args) {
         System.out.println("Hello, Happy World 2026!");
+        System.out.println("Hello, Happy World 2026!");
     }
 }
 
